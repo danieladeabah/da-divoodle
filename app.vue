677dd06d18ee119ba03c9037d1cb3d641ad83159
Daniel Adeabah <daniel.adeabah@amalitech.com>
2024-07-09 15:12:35 +0000

    feature: vercel speedinsights

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
 <template>
+  <SpeedInsights />
   <AppContainer>
     <NuxtPage />
   </AppContainer>
 </template>
+
+<script setup lang="ts">
+import { SpeedInsights } from "@vercel/speed-insights/nuxt"
+</script>
