e8c61711383f68a51e3257935ab27547db9aa4a2
Daniel Adeabah <daniel.adeabah@amalitech.com>
2024-07-11 11:08:53 +0000

    chore: install pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  plugins: ['~/plugins/pinia'],
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
   modules: ["nuxt-icon", "nuxt-snackbar", "@nuxt/ui"],
