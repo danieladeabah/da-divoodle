3d93a497c2fa3263ef7fb75fa1aa90185e881fc2
Daniel Adeabah <daniel.adeabah@amalitech.com>
2024-07-12 14:44:20 +0000

    refactor: survey store actions for readability and error handling

--- store/survey.ts
+++ store/survey.ts
@@ -7,25 +7,34 @@ export const useSurveyStore = defineStore("survey", {
   }),
   actions: {
     loadSurveys() {
-      const storedSurveys = localStorage.getItem("surveyData");
-      if (storedSurveys) {
-        this.surveys = JSON.parse(storedSurveys).surveys;
-      } else {
+      try {
+        const storedSurveys = localStorage.getItem("surveyData");
+        if (storedSurveys) {
+          this.surveys = JSON.parse(storedSurveys).surveys;
+        } else {
+          this.surveys = [];
+        }
+      } catch (error) {
+        console.error("Failed to load surveys:", error);
         this.surveys = [];
       }
     },
 
     saveSurvey(survey: Survey) {
-      const index = this.surveys.findIndex((s) => s.id === survey.id);
-      if (index !== -1) {
-        this.surveys.splice(index, 1, survey);
-      } else {
-        this.surveys.push(survey);
+      try {
+        const index = this.surveys.findIndex((s) => s.id === survey.id);
+        if (index !== -1) {
+          this.surveys.splice(index, 1, survey);
+        } else {
+          this.surveys.push(survey);
+        }
+        localStorage.setItem(
+          "surveyData",
+          JSON.stringify({ surveys: this.surveys })
+        );
+      } catch (error) {
+        console.error("Failed to save survey:", error);
       }
-      localStorage.setItem(
-        "surveyData",
-        JSON.stringify({ surveys: this.surveys })
-      );
     },
 
     getSurveyById(id: string): Survey | undefined {
@@ -33,13 +42,17 @@ export const useSurveyStore = defineStore("survey", {
     },
 
     addVote(surveyId: string, optionText: string) {
-      const survey = this.getSurveyById(surveyId);
-      if (survey) {
-        const option = survey.options.find((o) => o.text === optionText);
-        if (option) {
-          option.votes++;
-          this.saveSurvey(survey);
+      try {
+        const survey = this.getSurveyById(surveyId);
+        if (survey) {
+          const option = survey.options.find((o) => o.text === optionText);
+          if (option) {
+            option.votes++;
+            this.saveSurvey(survey);
+          }
         }
+      } catch (error) {
+        console.error("Failed to add vote:", error);
       }
     },
   },
