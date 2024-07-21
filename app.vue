43f91646e6e65c013229af0d724c5cfb8cc590e5
Daniel Adeabah <daniel.adeabah@amalitech.com>
2024-07-21 15:32:53 +0000

    chore: uninstall vercel speedinsights

--- app.vue
+++ app.vue
@@ -1,10 +1,5 @@
 <template>
-  <SpeedInsights />
   <Container>
     <NuxtPage />
   </Container>
 </template>
-
-<script setup lang="ts">
-import { SpeedInsights } from "@vercel/speed-insights/nuxt"
-</script>
